957f8ae10c017d931f41c0b9b0a54e3e2faabef0
salihduhan <salihduhansayar@gmail.com>
2022-03-17 16:08:12 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Çekirdeksiz Karpuz";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("...........");
+
+            }
+
+            Console.WriteLine("-------Metodlar-------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 15);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 25, 10);
+            sepetManager.Ekle2("Karpuz", "Çekirdeksiz Karpuz", 50, 10 );
+        }
+    }
+}
+
+
---
+++ SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodlar
+{
+    internal class SepetManager
+    {
+        public void Ekle (Urun urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi" + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi :" + urunAdi);
+        }
+    }
+
+}
